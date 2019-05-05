41aef409737a9c0c1a8762eef8dd5186bed0a809
anitapalash <647316392@github.com>
2019-05-05 12:14:58 +0300

    entrance init

---
+++ src/controllers/EntranceController.java
@@ -0,0 +1,24 @@
+package controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class EntranceController {
+    //логика входа в систему
+    
+    @FXML
+    private TextField loginField;
+
+    @FXML
+    private TextField passwordField;
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    void enterButtonPressed(ActionEvent event) {
+
+    }
+}
